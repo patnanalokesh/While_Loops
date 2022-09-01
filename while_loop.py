ca67daba10a87a211ad91a10c88c3057be1429a2
patnanalokesh <109522655+patnanalokesh@users.noreply.github.com>
2022-09-01 10:36:07 +0530

    Add files via upload

---
+++ while_loop.py
@@ -0,0 +1,22 @@
+# g=int(input("Enter Any Start Value:"))
+# h=int(input("Enter the End value:"))
+# while(g<=h):
+#     print(g)
+    # g+=1
+
+
+# a=int(input("Enter any Start value:"))
+# b=int(input("Enter the End value:"))
+# while(a>=b):
+#     print(a)
+#     a-=1
+
+# a=1
+# while(a<=10):
+#     print("Lokesh",a)
+#     a-=1
+
+# a=1
+# while(a<=10):
+#     print("Lokesh",a)
+#     a-=1
